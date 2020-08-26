5cb0d3e0c9999c436c232af39ebc96bca42bbef1
Atta Ullah Saeed <attaullahsaeed77@gmail.com>
2020-08-26 14:36:38 +0500

    Updates

---
+++ precache-manifest.549dc35c85fc4f35f44bc635a1ed0008.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17e358375388b0267fdcf06f2e186fe",
+    "url": "/netflix-app/index.html"
+  },
+  {
+    "revision": "c1435ab7c125a6228c56",
+    "url": "/netflix-app/static/css/main.1c772a7a.chunk.css"
+  },
+  {
+    "revision": "b29107247dff249313c6",
+    "url": "/netflix-app/static/js/2.bdec8f3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/netflix-app/static/js/2.bdec8f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1435ab7c125a6228c56",
+    "url": "/netflix-app/static/js/main.ed887b49.chunk.js"
+  },
+  {
+    "revision": "9fb6578a439907b33087",
+    "url": "/netflix-app/static/js/runtime-main.178ed97e.js"
+  }
+]);
